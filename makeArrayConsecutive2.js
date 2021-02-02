cdd45c240db89e2ae84a70ff86679e947f57b88b
Daniel Lee <daniejlee@gmail.com>
2021-02-01 20:18:16 -0800

    .

--- makeArrayConsecutive2.js
+++ makeArrayConsecutive2.js
@@ -1,5 +1,5 @@
 function makeArrayConsecutive2(statues) {
   let min = Math.min(...statues)
   let max = Math.max(...statues)
-  return max-min+1-statues.length
+  return max - min - statues.length + 1
 }
