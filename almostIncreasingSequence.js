34b50ce3ea90dc6c9a86273c37a8ea0e572f3e07
Daniel Lee <daniejlee@gmail.com>
2021-02-03 20:37:34 -0800

    clean

--- almostIncreasingSequence.js
+++ almostIncreasingSequence.js
@@ -1,31 +1,33 @@
 function almostIncreasingSequence(sequence) {
-  let wrongCounter = 0;
+  if(sequence.length <= 2){
+    return true
+  }
   if(sequence[0] > sequence[1]){
-    wrongCounter++
     sequence.splice(0, 1)
+    for (let i = 0; i < sequence.length; i++) {
+      if (sequence[i] <= sequence[i - 1]) {
+        return false
+      }
+    }
+    return true;
   }
 
   for(let i = 0; i < sequence.length; i++){
-    if(sequence[i] <= sequence[i-1] && sequence[i] <= sequence[i-2]){
-      sequence.splice(i, 1)
-      wrongCounter++
-      i--
+    if(sequence[i] <= sequence[i-1]){
+      console.log(sequence[i-2])
+      if (sequence[i] <= sequence[i - 2]){
+        sequence.splice(i, 1)
+      }
+      else if (sequence[i] > sequence[i - 2]){
+        sequence.splice(i-1, 1)
+      }
+      for(let j = 0; j < sequence.length; j++){
+        if (sequence[j] <= sequence[j-1]){
+          return false
+        }
+      }
+      return true;
     }
-    else if (sequence[i] <= sequence[i - 1] && sequence[i] > sequence[i - 2]){
-      sequence.splice(i-1, 1)
-      wrongCounter++
-      i--
-    }
-    else if (sequence[i] === sequence[i - 1]){
-      sequence.splice(i, 1)
-      wrongCounter++
-      i--
-    }
-
-    if (wrongCounter > 1) {
-      return false;
-    }
-
   }
   return true;
 }
