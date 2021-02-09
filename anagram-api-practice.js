cfce34b8cbdedc6959000b44fc50629d83903c99
Daniel Lee <daniejlee@gmail.com>
2021-02-08 23:50:45 -0800

    new

--- anagram-api-practice.js
+++
@@ -1,9 +0,0 @@
-let getAnagram = letters =>{
-console.log(letters)
-  fetch(`http://www.anagramica.com/all/:${letters}`)
-  .then(res => res.json())
-  .then(res => console.log(res))
-
-}
-
-getAnagram('word')
