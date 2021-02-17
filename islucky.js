87eb57488180ec0845fdd55dc4c99af80bb12470
Daniel Lee <daniejlee@gmail.com>
2021-02-16 21:08:11 -0800

    fix

--- islucky.js
+++ islucky.js
@@ -1,12 +1,12 @@
 function isLucky(n) {
+  let numString = n.toString(10)
   let firstHalfSum = 0
   let secondHalfSum = 0
-  for(let i = 0; i < n.length/2; i++){
-    firstHalfSum += n[i];
+  for(let i = 0; i < numString.length/2; i++){
+    firstHalfSum += +numString[i];
   }
-  for(let i = n.length/2; i < n.length; i++){
-    secondHalfSum += n[i];
+  for (let i = numString.length / 2; i < numString.length; i++){
+    secondHalfSum += +numString[i];
   }
-
   return (firstHalfSum === secondHalfSum)
 }
