23fcf8982a9e1e256a89b396acd406d2dd9c08d9
Daniel Lee <daniejlee@gmail.com>
2021-02-18 20:15:07 -0800

    1

--- sortByHeight.js
+++ sortByHeight.js
@@ -1,3 +1,15 @@
 function sortByHeight(a) {
+  let treePositions = [];
+  for(let i = a.length-1; i >= 0; i--){
+    if(a[i] === -1){
+      treePositions.unshift(i)
+      a.splice(i, 1)
+    }
+  }
 
+  a.sort((a, b) => a-b)
+  for(let i = 0; i < treePositions.length; i++){
+    a.splice(treePositions[i], 0, -1)
+  }
+  return a
 }
