8f4ffee3ea975167354dc5e4dcf4b9f03c260a18
Daniel Lee <daniejlee@gmail.com>
2021-02-24 17:40:35 -0800

    areSimilar

---
+++ areSimilar.js
@@ -0,0 +1,28 @@
+function areSimilar(a, b) {
+  let mismatches = 0;
+  let m1, m2;
+  let mismatchIndices = [];
+  for(let i = 0; i < a.length; i++){
+    if (a[i] !== b[i]){
+      mismatches++;
+      mismatchIndices.push(i);
+    }
+  }
+  console.log('mismatches: ', mismatches)
+
+
+  if(mismatches === 0){
+    return true
+  }
+  else if (mismatches > 2){
+    return false
+  }
+  else {
+    m1 = mismatchIndices[0];
+    m2 = mismatchIndices[1]
+    if(a[m1] === b[m2] && a[m2] === b[m1]){
+      return true;
+    }
+  }
+
+}
