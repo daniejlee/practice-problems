d3d2ce348a7ee935423904659b257d5083babaff
Daniel Lee <daniejlee@gmail.com>
2021-02-24 19:12:05 -0800

    arrayChange

--- arrayChange.js
+++ arrayChange.js
@@ -1,10 +1,10 @@
 function arrayChange(inputArray) {
- // let prev = inputArray[0];
   let moveCounter = 0;
-  for(let i = 1; i < inputArray.length; i++){
-    while(inputArray[i] < inputArray[i-1]){
+  for (let i = 1; i < inputArray.length; i++) {
+    while (inputArray[i] <= inputArray[i - 1]) {
       inputArray[i]++
       moveCounter++
     }
   }
+  return moveCounter
 }
