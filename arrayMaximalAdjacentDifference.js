50264e491d34461eb6834cbc23593d9f47edc76d
Daniel Lee <daniejlee@gmail.com>
2021-02-26 15:00:12 -0800

    simpler

--- arrayMaximalAdjacentDifference.js
+++ arrayMaximalAdjacentDifference.js
@@ -2,9 +2,12 @@ function arrayMaximalAdjacentDifference(inputArray) {
   let max = 0;
   for(let i = 0; i < inputArray.length; i++){
     let current = Math.abs(inputArray[i] - inputArray[i + 1])
-    max = (current > max)
-    ? current
-    : max;
+    if(current > max){
+      max = current
+    }
+    // max = (current > max)
+    // ? current
+    // : max;
   }
   return max
 }
