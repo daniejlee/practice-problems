e078e005afe32abc021ad08cfdcb73483e95a8f5
Daniel Lee <daniejlee@gmail.com>
2021-03-04 23:12:01 -0800

    fix

--- isIPv4Address.js
+++ isIPv4Address.js
@@ -24,32 +24,38 @@
 //   }
 // }
 
-let checkValid = (numString) => {
-  if(isNaN(numString)){
-    return false
-  }
-  if(numString[0] === '0' && numString[1]){
-    return false
-  }
-  let toNum = parseInt(numString, 10)
-  return toNum >= 0 && toNum <= 255
-}
+// let checkValid = (numString) => {
+//   if(isNaN(numString)){
+//     return false
+//   }
+//   if(numString[0] === '0' && numString[1]){
+//     return false
+//   }
+//   let toNum = parseInt(numString, 10)
+//   return toNum >= 0 && toNum <= 255
+// }
 
 
 
 function isIPv4Address(inputString) {
   let stringArray = inputString.split('.')
+  console.log(stringArray)
+  if(stringArray.length !== 4){
+    return false;
+  }
   for(let i = 0; i < stringArray.length; i++){
-    if (isNaN(stringArray[i])) {
+    console.log(isNaN(stringArray[i]))
+    if (isNaN(stringArray[i]) || stringArray[i] === '') {
       return false
     }
     if (stringArray[i][0] === '0' && stringArray[i][1]) {
       return false
     }
     let toNum = parseInt(stringArray[i], 10)
-    return toNum >= 0 && toNum <= 255
+    if(toNum < 0 || toNum > 255){
+      return false
+    }
   }
-
-  }
+  return true
 
 }
