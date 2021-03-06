39770e7b2ae11cd6fb1731a2c6d3cd7b315762e4
Daniel Lee <daniejlee@gmail.com>
2021-03-05 20:18:43 -0800

    logs

--- boxBlur.js
+++ boxBlur.js
@@ -4,9 +4,11 @@ function boxBlur(image) {
 
   let blurredImage = [];
   let innerArray = []
+  console.log(image)
 
   //height of new Array
   for(let i = 0; i < imageHeight - 2; i++){
+
     //sets length of new Array
     for(let j = 0; j < imageLength - 2; j++){
       let innerSquareSum = 0;
@@ -15,11 +17,17 @@ function boxBlur(image) {
         image[i + 1][j] + image[i + 1][j + 1] + image[i + 1][j + 2]+
         image[i + 2][j] + image[i + 2][j + 1] + image[i + 2][j + 2]
         )
-      innerArray.push(Math.floor(innerSquareSum/9))
+        console.log(innerSquareSum)
+        innerArray.push(Math.floor(innerSquareSum/9))
     }
-
     blurredImage.push(innerArray)
-    innerArray = []
+    console.log(blurredImage)
   }
   return blurredImage
 }
+
+
+
+// image[0][0] + image[0][1] + image[0][2] +
+// image[1][0] + image[1][1] + image[1][2] +
+// image[2][0] + image[2][1] + image[2][2]
