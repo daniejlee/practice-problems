a5f7e5c0a9b5a4c494b987d49d1d59226fbc344a
Daniel Lee <daniejlee@gmail.com>
2021-03-10 23:45:10 -0800

    ..

--- minesweeper.js
+++ minesweeper.js
@@ -1,32 +1,78 @@
 function minesweeper(matrix) {
-  let gridLength = matrix[0][0].length
-  let gridHeight = matrix[0].length
+  let gridLength = matrix[0].length
+  let gridHeight = matrix.length
+  console.log(gridLength)
+  console.log(gridHeight)
   let newMatrix = [];
+  let innerArray = [];
 
 
   for(let i = 0; i < gridHeight; i++){
 
     for(let j = 0; j < gridLength; j++){
+     let singleBox = 0;
 
+     //
+      if (matrix[i - 1][j - 1] === true){
+       singleBox++
+     }
+     if(matrix[i-1][j] === true){
+       singleBox++
+     }
+     if(matrix[i-1][j+1] === true){
+       singleBox++
+     }
+     //
+      if (matrix[i][j - 1] === true) {
+        singleBox++
+      }
+      if (matrix[i][j + 1] === true) {
+        singleBox++
+      }
+      //
+      if (matrix[i + 1][j - 1] === true) {
+        singleBox++
+      }
+      if (matrix[i + 1][j] === true) {
+        singleBox++
+      }
+      if (matrix[i + 1][j + 1] === true) {
+        singleBox++
+      }
 
+      innerArray.push(singleBox)
+      console.log(innerArray)
     }
+    newMatrix.push(innerArray)
+    innerArray = [];
   }
+  return newMatrix
 }
 
 
 
 /*
-for (let i = 0; i < imageHeight - 2; i++) {
+function boxBlur(image) {
+  let imageLength = image[0].length
+  let imageHeight = image.length
 
-  for (let j = 0; j < imageLength - 2; j++) {
-    let innerSquareSum = (
-      image[i][j] + image[i][j + 1] + image[i][j + 2] +
-      image[i + 1][j] + image[i + 1][j + 1] + image[i + 1][j + 2] +
-      image[i + 2][j] + image[i + 2][j + 1] + image[i + 2][j + 2]
-    )
-    innerArray.push(Math.floor(innerSquareSum / 9))
+  let blurredImage = [];
+  let innerArray = []
+
+  for (let i = 0; i < imageHeight - 2; i++) {
+
+    for (let j = 0; j < imageLength - 2; j++) {
+      let innerSquareSum = (
+        image[i][j] + image[i][j + 1] + image[i][j + 2] +
+        image[i + 1][j] + image[i + 1][j + 1] + image[i + 1][j + 2] +
+        image[i + 2][j] + image[i + 2][j + 1] + image[i + 2][j + 2]
+      )
+      innerArray.push(Math.floor(innerSquareSum / 9))
+    }
+    blurredImage.push(innerArray)
+    innerArray = []
   }
-  blurredImage.push(innerArray)
-  innerArray = []
+  return blurredImage
 }
+
 */
