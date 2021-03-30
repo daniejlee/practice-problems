480c80f135eba96a2039948182df66f48a663cb4
Daniel Lee <daniejlee@gmail.com>
2021-03-29 18:26:55 -0700

    alphaShift

--- alphabeticShift.js
+++ alphabeticShift.js
@@ -1,22 +1,14 @@
-function matrixElementsSum(matrix) {
-  let sum = 0;
-  let hauntedColumns = [];
-  for(let i = 0; i < matrix.length; i++){
-
-    for(let j = 0; j < matrix[i].length; j++){
-      if(hauntedColumns.includes(j)){
-        continue;
-      }
-
-      if(matrix[i][j] === 0){
-        if(!hauntedColumns.includes(j)){
-          hauntedColumns.push(j)
-        }
-      }
-      else{
-        sum += matrix[i][j]
-      }
+function alphabeticShift(inputString) {
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  let newWord = '';
+  for (let i = 0; i < inputString.length; i++){
+    if(inputString[i] === 'z'){
+      newWord += 'a'
+    }
+    else{
+      let alphaIndex = alphabet.indexOf(inputString[i]);
+      newWord += alphabet[alphaIndex + 1]
     }
   }
-  return sum;
+  return newWord
 }
