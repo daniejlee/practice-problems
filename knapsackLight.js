5297191b7b8e23917fd9eed47e75c506b527876c
Daniel Lee <daniejlee@gmail.com>
2021-04-06 22:58:18 -0700

    clean'

--- knapsackLight.js
+++ knapsackLight.js
@@ -1,19 +1,6 @@
 function knapsackLight(value1, weight1, value2, weight2, maxW) {
-  if(weight1 > maxW && weight2 > maxW){
-    return 0
-  }
   if (weight1 + weight2 <= maxW) {
     return value1 + value2;
-  }
-
-  let item1 = {
-    value: value1,
-    weight: weight1,
-  }
-
-  let item2 = {
-    value: value2,
-    weight: weight2,
   }
 
   let value = 0;
