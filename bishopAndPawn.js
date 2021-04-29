76c6afecf83b0a217da19635ac48de89ec37e5cc
Daniel Lee <daniejlee@gmail.com>
2021-04-28 23:09:44 -0700

    bishopAndPawn

--- bishopAndPawn.js
+++ bishopAndPawn.js
@@ -12,14 +12,50 @@ function bishopAndPawn(bishop, pawn) {
 let bishopPosition = [alphaNum[bishop[0]], parseInt(bishop[1], 10)];
 let pawnPosition = [alphaNum[pawn[0]], parseInt(pawn[1], 10)];
 
-console.log(bishopPosition, pawnPosition)
 
-if(bishop[0] > pawn[0]){
-    console.log('left')
-
+  if(bishop[0] > pawn[0]){
+    if (pawn[1] > bishop[1]) {
+      for (let i = bishop[1]; i <= 8; i++) {
+        bishopPosition[0]--
+        bishopPosition[1]++
+        if (bishopPosition[0] === pawnPosition[0] && bishopPosition[1] === pawnPosition[1]) {
+          return true;
+        }
+      }
+    }
+    else if (pawn[1] < bishop[1]) {
+      for (let i = bishop[1]; i >= 0; i--) {
+        bishopPosition[0]--
+        bishopPosition[1]--
+        if (bishopPosition[0] === pawnPosition[0] && bishopPosition[1] === pawnPosition[1]) {
+          return true;
+        }
+      }
+    }
   }
+
+
+
   else if(bishop[0] < pawn[0]){
-    console.log('right')
-
+    if(pawn[1] > bishop[1]){
+      for(let i = bishop[1]; i <= 8; i++){
+        bishopPosition[0]++
+        bishopPosition[1]++
+        if(bishopPosition[0] === pawnPosition[0] && bishopPosition[1] === pawnPosition[1]){
+          return true;
+        }
+      }
+    }
+    else if (pawn[1] < bishop[1]) {
+      for (let i = bishop[1]; i >= 0; i--) {
+        bishopPosition[0]++
+        bishopPosition[1]--
+        if (bishopPosition[0] === pawnPosition[0] && bishopPosition[1] === pawnPosition[1]) {
+          return true;
+        }
+      }
+    }
   }
+  return false;
+
 }
