6962321ca76720b7c4b9d974f77d46be14612993
Daniel Lee <daniejlee@gmail.com>
2021-05-11 21:58:50 -0700

    clean

--- isMac48Address.js
+++ isMac48Address.js
@@ -2,24 +2,7 @@ function isMAC48Address(inputString) {
   if(inputString.length !== 17){
     return false;
   }
-  // for (let i = 2; i < inputString.length; i += 3) {
-  //   if (inputString[i] !== '-') {
-  //     return false
-  //   }
-  // }
 
-  // let newStr = inputString.replaceAll('-', '')
-  // console.log(newStr)
-
-  // for (let i = 0; i < newStr.length; i++) {
-  //   console.log('i', i)
-  //   if (!newStr[i].match(/[A-F0-9]/i)) {
-  //     return false;
-  //   }
-  // }
-  // return true
-
-// console.log(inputString.length)
 for(let i = 0; i < inputString.length; i++){
   if((i - 2) % 3 === 0){
     console.log('here', i)
