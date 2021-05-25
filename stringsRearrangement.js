d261d4554bd933a1c30747b4b17be36614169e9e
Daniel Lee <daniejlee@gmail.com>
2021-05-24 23:56:02 -0700

    stringsRearrangement2

--- stringsRearrangement.js
+++ stringsRearrangement.js
@@ -36,3 +36,27 @@ function checkOneCharDifference2(array) {
 
   return true
 }
+
+function palindromeRearranging(inputString) {
+  let arr = inputString.split('').sort();
+  let numOdd = 0;
+  let charCount = {};
+
+  arr.forEach((character) => {
+    if (character in charCount) {
+      charCount[character] += 1;
+    }
+    else {
+      charCount[character] = 1;
+    }
+  })
+  for (const char in charCount) {
+    if (charCount[char] % 2 === 1) {
+      numOdd++
+      if (numOdd > 1) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
