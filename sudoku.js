8edb5246fc895bc76f2b6164fb0af4723dc77fe9
Daniel Lee <daniejlee@gmail.com>
2021-05-28 22:30:37 -0700

    sudoku

--- sudoku.js
+++ sudoku.js
@@ -31,6 +31,45 @@ function sudoku(grid) {
   }
 
 
+  //check boxes
+  let box1 = [];
+  let box2 = [];
+  let box3 = [];
+  for (let i = 0; i < 9; i++) {
+    if(i === 3 || i === 6){
+      box1 = [];
+      box2 = [];
+      box3 = [];
+    }
+    for (let j = 0; j < 9; j++) {
+      let num = grid[i][j]
+      if(j < 3){
+        if (box1.includes(num)) {
+          return false;
+        }
+        else {
+          box1.push(num)
+        }
+      }
+      else if(j < 6){
+        if (box2.includes(num)) {
+          return false;
+        }
+        else {
+          box2.push(num)
+        }
+      }
+      else if(j < 9){
+        if (box3.includes(num)) {
+          return false;
+        }
+        else {
+          box3.push(num)
+        }
+      }
+    }
+  }
+
 
   return true;
 }
