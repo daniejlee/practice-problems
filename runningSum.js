157a03d9b5e7d6e984e55bcac603f6d88f24fa23
Daniel Lee <daniejlee@gmail.com>
2021-06-02 19:18:41 -0700

    runningSum

--- runningSum.js
+++ runningSum.js
@@ -1,49 +1,13 @@
-function reverseInParentheses(inputString) {
-  let stringArray = inputString.split("")
-  console.log(stringArray)
-  let leftIndex, rightIndex;
-  let leftCount = 0;
-
-
-  console.log(stringArray.length)
-  for(let i = 0; i < stringArray.length; i++){
-    if(stringArray[i] === '('){
-      if(leftCount === 0){
-        leftIndex = i;
-        leftCount++
-      }
-      else if(leftCount > 0){
-        leftCount++
-      }
-    }
-
-
-    else if(stringArray[i] === ')'){
-      if(leftCount === 1){
-        rightIndex = i;
-        stringArray = reverseWords(leftIndex, rightIndex, stringArray)
-        i = 0;
-        leftCount = 0;
-      }
-      else {
-        leftCount--;
-      }
-    }
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function (nums) {
+  let sum = 0;
+  let arr = [];
+  for(let i=0; i< nums.length; i++){
+    sum += nums[i];
+    arr.push(sum);
   }
-  return stringArray.join('')
-}
-
-
-let reverseWords = (left, right, word) => {
-  let wordInParenthesesReversed = word.slice(left + 1, right).reverse().map(x => {
-    if(x === ')'){
-      return '('
-    }
-    else if(x === '('){
-      return ')'
-    }
-    else return x
-  })
-  word.splice(left, (right - left + 1), ...wordInParenthesesReversed)
-  return word
-}
+  return arr;
+};
