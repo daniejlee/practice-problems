efe42c5412f6de116245955097506d30aad24a30
Daniel Lee <daniejlee@gmail.com>
2021-06-03 18:02:45 -0700

    lengthOfLastWord

--- lengthOfLastWord.js
+++ lengthOfLastWord.js
@@ -2,27 +2,8 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function(s) {
-  let count = 0;
-  let end = true;
+// var lengthOfLastWord = function(s) {
+//     return s.trim().split(' ').pop().length;
+// };
 
-    // while(end){
-    //   count++;
-    //   if(s[i] === ' '){
-    //     end = false;
-    //   }
-    //   i--
-    // }
-    // return 0;
-
-    // let arr = s.split('')
-    // while(arr.slice(-1) === ' '){
-    //   arr.pop()
-    // }
-    // let yes =  arr.join('').split(' ');
-    // console.log(yes)
-
-    let trimmed = s.trim();
-    let arr = trimmed.split('');
-    return arr.pop().length
-};
+const lengthOfLastWord = s => s.trim().split(' ').pop().length;
