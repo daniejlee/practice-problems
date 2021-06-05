b17c8a1555ead33e303c153dd0ef3873f392a47b
Daniel Lee <daniejlee@gmail.com>
2021-06-04 23:52:42 -0700

    .

--- plusOne.js
+++ plusOne.js
@@ -3,20 +3,20 @@
  * @return {number[]}
  */
 var plusOne = function (digits) {
-  // let reverseArray = digits.reverse()
-  // i = 0;
-  // reverseArray[i] += 1;
-  // while (reverseArray[i] > 9) {
-  //   reverseArray[i] = 0;
-  //   if (reverseArray[i + 1]) {
-  //     reverseArray[i + 1] += 1
-  //   } else {
-  //     reverseArray[i + 1] = 1
-  //   }
-  //   i++
-  // }
+  let reverseArray = digits.reverse()
+  i = 0;
+  reverseArray[i] += 1;
+  while (reverseArray[i] > 9) {
+    reverseArray[i] = 0;
+    if (reverseArray[i + 1]) {
+      reverseArray[i + 1] += 1
+    } else {
+      reverseArray[i + 1] = 1
+    }
+    i++
+  }
 
-  // return reverseArray.reverse()
+  return reverseArray.reverse()
 
   console.log(digits.join('').toString())
   console.log(parseInt(digits.join('').toString(), 10))
