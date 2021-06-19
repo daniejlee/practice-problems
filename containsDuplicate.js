3664ccaa7599879cd609c85f5eccb0c9bd9e5697
Daniel Lee <daniejlee@gmail.com>
2021-06-18 23:24:35 -0700

    containsDupe

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -3,11 +3,23 @@
  * @param {number} n
  * @return {number[]}
  */
-var shuffle = function(nums, n) {
-  let arr = []
-    for(let i = 0; i < n; i++){
-      arr.push(nums[i])
-      arr.push(nums[i + n])
+var containsDuplicate = function (nums) {
+  let obj = {};
+  for (let i of nums) {
+    obj[i] = obj[i] ? obj[i] + 1 : obj[i] = 1;
+  }
+  let keys = Object.keys(obj);
+  let count = 0;
+  for (let i = 0; i < keys.length; i++) {
+    if (obj[keys[i]] >= 2) {
+      count++;
     }
-    return arr
+  }
+  if (count > 0) {
+    return true;
+  }
+  if (count === 0) {
+    return false;
+  }
+
 };
