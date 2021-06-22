0a4a2347a0f91abe7cf01f61de3b91818f111756
Daniel Lee <daniejlee@gmail.com>
2021-06-21 20:59:29 -0700

    maxWealth

--- maximumWealth.js
+++ maximumWealth.js
@@ -3,7 +3,12 @@
  * @return {number}
  */
 var maximumWealth = function(accounts) {
+  let maxWealth = 0;
   for(let i= 0; i < accounts.length; i++){
     let sum = accounts[i].reduce((acc, curr) => acc + curr)
+    if (sum > maxWealth){
+      maxWealth = sum;
+    }
   }
+  return maxWealth
 };
