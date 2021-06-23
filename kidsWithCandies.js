29194cb4ffa878c2094cac9c63f6ba064b718754
Daniel Lee <daniejlee@gmail.com>
2021-06-22 21:40:40 -0700

    kidsWithCandies

--- kidsWithCandies.js
+++ kidsWithCandies.js
@@ -4,7 +4,14 @@
  * @return {boolean[]}
  */
 var kidsWithCandies = function(candies, extraCandies) {
-    let max = Math.max(...candies)
-    console.log(max)
-    return candies.map(numCandies => numCandies + extraCandies > max)
+  let max = 0;
+  let arr = [];
+  for(let i = 0; i < candies.length; i++){
+    candies[i] > max ? max = candies[i] : ''
+  }
+
+  for(let i = 0; i < candies.length; i++){
+    candies[i] + extraCandies >= max ? arr.push(true) : arr.push(false)
+  }
+  return arr
 };
